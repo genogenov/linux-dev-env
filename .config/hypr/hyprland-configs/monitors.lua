948e39d64782e9963b6bcd764603ed704065c947
GGenov <12395610+genogenov@users.noreply.github.com>
2026-05-12 03:50:32 -0700

    cleanup

--- .config/hypr/hyprland-configs/monitors.lua
+++ .config/hypr/hyprland-configs/monitors.lua
@@ -8,16 +8,16 @@ hl.monitor({output=Monitor1, mode="preferred", position="0x0", scale="1.5"})
 hl.monitor({output=Monitor2, mode="preferred", position="2560x0", scale="1.5"})
 hl.monitor({output=Monitor3, mode="preferred", position="5120x700", scale="1"})
 
----@param monitor HL.Monitor
-local arrangeMonitor = function (monitor)
-    if monitor.name == Monitor1 then
-        hl.monitor({output=Monitor1, mode="preferred", position="0x0", scale="1.5"})
-    elseif monitor.name == Monitor2 then
-        hl.monitor({output=Monitor2, mode="preferred", position="2560x0", scale="1.5"})
-    elseif monitor.name == Monitor3 then
-        hl.monitor({output=Monitor3, mode="preferred", position="5120x700", scale="1"})
-    end
-end
+-- ---@param monitor HL.Monitor
+-- local arrangeMonitor = function (monitor)
+--     if monitor.name == Monitor1 then
+--         hl.monitor({output=Monitor1, mode="preferred", position="0x0", scale="1.5"})
+--     elseif monitor.name == Monitor2 then
+--         hl.monitor({output=Monitor2, mode="preferred", position="2560x0", scale="1.5"})
+--     elseif monitor.name == Monitor3 then
+--         hl.monitor({output=Monitor3, mode="preferred", position="5120x700", scale="1"})
+--     end
+-- end
 
-hl.on("monitor.added", arrangeMonitor)
-hl.on("monitor.removed", arrangeMonitor)
+-- hl.on("monitor.added", arrangeMonitor)
+-- hl.on("monitor.removed", arrangeMonitor)
